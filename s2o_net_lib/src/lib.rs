328c9098e31e3f21e0fd2e8c1fdeb146a55466b8
zv9games <gregor.whitelight@gmail.com>
2025-01-01 15:16:16 -0500

    building backend functions

--- s2o_net_lib/src/lib.rs
+++ s2o_net_lib/src/lib.rs
@@ -1,5 +1,12 @@
+// lib.rs
+pub mod bootup;
 pub mod block_all;
 pub mod data_speeds;
-pub mod packet_scan;
 pub mod menu;
 pub mod permissions;
+pub mod admin_menu;
+pub mod ds_menu;
+pub mod pc_menu;
+pub mod analytics_menu;
+// Remove or comment out the following line
+// pub mod packet_scan;
--- s2o_net_lib/src/packet_scan.rs
+++
@@ -1,4 +0,0 @@
-pub fn packet_scan() {
-    println!("Packet scanning functionality.");
-    // Add the actual code for packet scanning here
-}
